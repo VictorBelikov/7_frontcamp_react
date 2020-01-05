29297c15791e0cd4129900d0499e1bf9c316cbdb
Victor Belikov <v1ctor.belikov@gmail.com>
2020-01-05 14:56:16 +0300

    style: add styles for <Movie /> component

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import movieImg from '../../assets/images/dummyImage.png';
 import style from './Movie.module.scss';
+import { Link } from 'react-router-dom';
 
 const Movie = () => {
   return (
-    <div className={style.movie}>
-
-    </div>
+    <Link className={style.movie} to={'/movie'}>
+      <img src={movieImg} alt="Cover" />
+      <div className={style.metricsWrapper}>
+        <span className={style.itemTitle}>Kill Bill: Vol 2</span>
+        <span className={style.itemRelease}>2003</span>
+      </div>
+      <span className={style.itemGenre}>Action & Adventure</span>
+    </Link>
   );
 };
 
