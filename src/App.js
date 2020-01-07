0201a4060fa3173904baabd9d0cbd61f7a99ace7
Victor Belikov <v1ctor.belikov@gmail.com>
2020-01-07 19:05:39 +0300

    feat: add <ErrorBoundary /> component

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ import SearchResult from './Containers/SearchResult/SearchResult';
 import Footer from './Containers/Footer/Footer';
 import Brand from './Components/UI/Brand/Brand';
 import Error from './Components/Error/Error';
+import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   render() {
     return (
-      <div className={style.mainWrapper}>
-        <Switch>
-          <Route path={'/movie/:id'} component={ParticularMovie} />
-          <Route path={'/search'} component={Search} />
-          <Route path={'/'} exact component={Search} />
-          <Route path={'/'} component={Error} />
-        </Switch>
-        <SearchResult />
-        <Footer>
-          <Brand fWord={'netflix'} sWord={'roulette'} />
-        </Footer>
-      </div>
+      <ErrorBoundary>
+        <div className={style.mainWrapper}>
+          <Switch>
+            <Route path={'/movie/:id'} component={ParticularMovie} />
+            <Route path={'/search'} component={Search} />
+            <Route path={'/'} exact component={Search} />
+            <Route path={'/'} component={Error} />
+          </Switch>
+          <SearchResult />
+          <Footer>
+            <Brand fWord={'netflix'} sWord={'roulette'} />
+          </Footer>
+        </div>
+      </ErrorBoundary>
     );
   }
 }
--- src/store/actions/films.test.js
+++ src/store/actions/films.test.js
@@ -38,20 +38,20 @@ describe('actions', () => {
 });
 
 describe('async actions', () => {
-  afterEach(() => {
-    fetchMock.restore();
-  });
-
   it('creates FETCH_TODOS_SUCCESS when fetching todos has been done', () => {
     fetchMock.getOnce('/todos', {
       body: { todos: ['do something'] },
       headers: { 'content-type': 'application/json' },
     });
+
     const expectedActions = [{ type: __types.FETCH_FILMS_SUCCESS, body: { todos: ['do something'] } }];
     const store = mockStore({ todos: [] });
-    return store.dispatch(__actions.fetchFilms()).then(() => {
-      // return of async actions
-      expect(store.getActions()).toEqual(expectedActions);
-    });
+
+    return store
+      .dispatch(__actions.fetchFilms('search=drama&searchBy=genres&sortOrder=desc&sortBy=vote_average&limit=20'))
+      .then(() => {
+        // return of async actions
+        expect(store.getActions()).toEqual(expectedActions);
+      });
   });
 });
